6986bf5557d4152b0915e84dcaf240c353dd7ae0
Sergey Fortis <s_fortis@mail.ru>
2021-08-04 18:31:46 +0500

    Fragment - All done

--- app/src/main/java/ru/netology/SinglePostFragment.kt
+++ app/src/main/java/ru/netology/SinglePostFragment.kt
@@ -30,12 +30,67 @@ class SinglePostFragment : Fragment() {
         )
         println(arguments?.getInt("MyId"))
 
-        viewModel.data.observe(viewLifecycleOwner) {
+
+        val post = viewModel.data.value
+            ?.find { it.id == arguments?.getInt("MyId")
+                }
+        binding.apply{
+            like.setOnClickListener {
+                like.text = post?.likesCount?.let { it -> transferToK(it) }
+                if (post != null) {
+                    viewModel.like(post.id)
+
+                }
+
+            }
+
+            shared.setOnClickListener {
+                val intent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    if (post != null) {
+                        putExtra(Intent.EXTRA_TEXT, post.content)
+                    }
+                    type = "text/plain"
+                }
+
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                startActivity(shareIntent)
+                if (post != null) {
+                    viewModel.share(post.id)
+                }
+            }
+
+            menu.setOnClickListener {
+                PopupMenu(it.context, it).apply {
+                    inflate(R.menu.menu_post)
+                    setOnMenuItemClickListener { menuItem ->
+                        when (menuItem.itemId) {
+                            R.id.remove_post -> {
+                                findNavController().navigate(R.id.action_singlePostFragment_to_feedFragment)
+                                true
+                            }
+                            R.id.edit_post -> {
+                                val bundle = Bundle()
+                                bundle.putString("MyArg", post?.content)
+                                post?.let { it1 -> viewModel.edit(it1) }
+                                findNavController().navigate(
+                                    R.id.action_singlePostFragment_to_editPostFragment,
+                                    bundle
+                                )
+                                true
+                            }
+                            else -> false
+                        }
+                    }
+                }.show()
+            }
+        }
+
+                viewModel.data.observe(viewLifecycleOwner) {
             val post = viewModel.data.value
                 ?.find { it.id == arguments?.getInt("MyId")
-
                     }
-
 
 
             binding.apply {
@@ -49,59 +104,7 @@ class SinglePostFragment : Fragment() {
                 if (post?.video == null) play.visibility = View.INVISIBLE
                 else View.VISIBLE
 
-
-                like.setOnClickListener {
-                    like.text = post?.likesCount?.let { it -> transferToK(it) }
-                    if (post != null) {
-                        viewModel.like(post.id)
-
-                    }
-
-                }
-
-                shared.setOnClickListener {
-                    val intent = Intent().apply {
-                        action = Intent.ACTION_SEND
-                        if (post != null) {
-                            putExtra(Intent.EXTRA_TEXT, post.content)
-                        }
-                        type = "text/plain"
-                    }
-
-                    val shareIntent =
-                        Intent.createChooser(intent, getString(R.string.chooser_share_post))
-                    startActivity(shareIntent)
-                    if (post != null) {
-                        viewModel.share(post.id)
-                    }
-                }
-
-                menu.setOnClickListener {
-                    PopupMenu(it.context, it).apply {
-                        inflate(R.menu.menu_post)
-                        setOnMenuItemClickListener { menuItem ->
-                            when (menuItem.itemId) {
-                                R.id.remove_post -> {
-                                    findNavController().navigate(R.id.action_singlePostFragment_to_feedFragment)
-                                    true
-                                }
-                                R.id.edit_post -> {
-                                    val bundle = Bundle()
-                                    bundle.putString("MyArg", post?.content)
-                                    post?.let { it1 -> viewModel.edit(it1) }
-                                    findNavController().navigate(
-                                        R.id.action_singlePostFragment_to_editPostFragment,
-                                        bundle
-                                    )
-                                    true
-                                }
-                                else -> false
-                            }
-                        }
-                    }.show()
-                }
             }
-
         }
         return binding.root
     }
